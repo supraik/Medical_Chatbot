9213d055057ba91a536b595da3da0ccef5c38fbb
Supratik Mukherjee <supratiksm10@gmail.com>
2025-09-24 21:31:57 +0530

    Initial project setup: add PDF loader, data extraction, and environment config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rawat_X_Supra_X_Khanna",
+    author_email="supratiksm10@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
